2d2c28cf9f97d48ba35a8c102a5843a04ad58837
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-25 11:22:50 +0200

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projet noté
+
+## Commandes
+
+npm install
+
+### Run l'app
+
+npm run dev
+
+### Tests e2e
+
+npm run e2e
+
+### Tests
+
+npm run vitest
--- e2e/home.spec.js
+++ e2e/home.spec.js
@@ -1,10 +1,12 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("visual regression (ignore la première board)", async ({ page }) => {
-  // on masque la board dynamique pour ne pas tenir compte des dés tirés
-  await expect(page).toHaveScreenshot("home-page.png", {
-    fullPage: true,
-    maxDiffPixelRatio: 0.02,
+test.describe("Page home", () => {
+  test("visual regression (ignore la première board)", async ({ page }) => {
+    // on masque la board dynamique pour ne pas tenir compte des dés tirés
+    await expect(page).toHaveScreenshot("home-page.png", {
+      fullPage: true,
+      maxDiffPixelRatio: 0.02,
+    });
   });
 });
