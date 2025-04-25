c35a7ca7a0908c008fa3b5b8db6abdbd74208b76
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-25 10:32:01 +0200

    get time

--- src/__tests__/clock.test.js
+++ src/__tests__/clock.test.js
@@ -1,6 +1,6 @@
 // tests/clock.js
 import { describe, expect, it } from "vitest";
-import { computeTime } from "../modules/clock";
+import { computeTime, getTime } from "../modules/clock";
 
 describe("compute time", () => {
   it("should return mortin if value is <= 2", () => {
@@ -14,5 +14,49 @@ describe("compute time", () => {
   });
   it("should return nuight if value is > 5", () => {
     expect(computeTime(6)).toBe("nuight");
+  });
+});
+
+describe("get time", () => {
+  describe("tests individuels", () => {
+    describe("valeur 1", () => {
+      it("should return -1 si la lune est 1 et les autres désactivés", () => {
+        expect(getTime(1, 0, 0)).toBe(-1);
+      });
+      it("should return 1 si le soleil est 1 et les autres désactivés", () => {
+        expect(getTime(0, 1, 0)).toBe(1);
+      });
+      it("should return 3 si la terre est 1 et les autres désactivés", () => {
+        expect(getTime(0, 0, 1)).toBe(3);
+      });
+    });
+    describe("valeur 2", () => {
+      it("should return 1 si la lune est 2 et les autres désactivés", () => {
+        expect(getTime(2, 0, 0)).toBe(1);
+      });
+      it("should return 2 si le soleil est 2 et les autres désactivés", () => {
+        expect(getTime(0, 2, 0)).toBe(2);
+      });
+      it("should return 6 si la terre est 2 et les autres désactivés", () => {
+        expect(getTime(0, 0, 2)).toBe(6);
+      });
+    });
+  });
+  describe("tests spécifiques", () => {
+    it("should return 0 if they are all disabled", () => {
+      expect(getTime(0, 0, 0)).toBe(0);
+    });
+    it("should return le total divisé par 2 arrondi à l'entier inférieur si la lune est 2", () => {
+      expect(getTime(2, 1, 1)).toBe(1);
+    });
+    it("should return 6 si le soleil n'est pas 1 et la terre est 2", () => {
+      expect(getTime(0, 2, 2)).toBe(6);
+    });
+    it("should return soleil + cadran *2 + terre si le soleil est 2 et la terre est 1", () => {
+      expect(getTime(0, 2, 1)).toBe(2 + 1 * 2 + 2);
+    });
+    it("should not count terre if soleil is 1", () => {
+      expect(getTime(0, 1, 2)).toBe(1);
+    });
   });
 });
--- src/modules/clock.js
+++ src/modules/clock.js
@@ -1,5 +1,76 @@
+let values = {
+  lune: {
+    0: {
+      cadran: 0,
+      power: 0,
+    },
+    1: {
+      cadran: 1,
+      power: -2,
+    },
+    2: {
+      cadran: 2,
+      power: 0,
+    },
+  },
+  soleil: {
+    0: {
+      cadran: 0,
+      power: 0,
+    },
+    1: {
+      cadran: 1,
+      power: 0,
+    },
+    2: {
+      cadran: 2,
+      power: 0,
+    },
+  },
+  terre: {
+    0: {
+      cadran: 0,
+      power: 0,
+    },
+    1: {
+      cadran: 1,
+      power: 2,
+    },
+    2: {
+      cadran: 2,
+      power: 6,
+    },
+  },
+};
 export function getTime(lune, soleil, terre) {
-  return 0;
+  if (lune + terre + soleil === 0) {
+    return 0;
+  }
+  if (soleil != 1 && terre === 2) {
+    return 6;
+  }
+  if (soleil === 1 && terre != 0) {
+    return getTime(lune, soleil, 0);
+  }
+  let total = 0;
+  // terre
+  total += terre;
+  if (terre === 1) {
+    total += 2;
+  }
+  // soleil
+  total += soleil;
+  if (soleil === 2) {
+    total += terre;
+  }
+  // lune
+  total += lune;
+  if (lune === 1) {
+    total -= 2;
+  } else if (lune === 2) {
+    total = Math.floor(total / 2);
+  }
+  return total;
 }
 export function computeTime(value) {
   if (value <= 2) {
