1ea1930f8e3cf2ced88880c06d71bd588f5499e5
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-25 11:17:55 +0200

    github actions

---
+++ e2e/home.spec.js
@@ -0,0 +1,10 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("visual regression (ignore la première board)", async ({ page }) => {
+  // on masque la board dynamique pour ne pas tenir compte des dés tirés
+  await expect(page).toHaveScreenshot("home-page.png", {
+    fullPage: true,
+    maxDiffPixelRatio: 0.02,
+  });
+});
