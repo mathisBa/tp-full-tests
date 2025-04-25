95809b81fa4f28e536cd35ce6c738b8b9a7e9544
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-25 09:34:58 +0200

    dev func computeTime

--- src/modules/clock.js
+++ src/modules/clock.js
@@ -2,5 +2,12 @@ export function getTime(lune, soleil, terre) {
   return 0;
 }
 export function computeTime(value) {
+  if (value <= 2) {
+    return "mortin";
+  } else if (value <= 4) {
+    return "aprenoon";
+  } else if (value <= 5) {
+    return "soirning";
+  }
   return "nuight";
 }
